dd91f17154b61fbc31a7b0e601bfdc623c9464ed
Patricia Ruffino <65633304+PatriciaRuffino@users.noreply.github.com>
2022-06-04 15:44:04 -0300

    Delete hr-worker directory

--- hr-worker/src/main/java/com/devsuperior/repository/WorkerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.repository;
-
-import com.devsuperior.entities.Worker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkerRepository extends JpaRepository <Worker, Long> {
-}
